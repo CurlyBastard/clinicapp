1571aa5357d349326c1d03ad7515d4ada1388071
curlybastard <onc.hub99@gmail.com>
2023-09-15 02:48:27 +0300

    fixed dashboards

--- app/dashboards/category_dashboard.rb
+++ app/dashboards/category_dashboard.rb
@@ -40,7 +40,7 @@ class CategoryDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-    doctors
+    name
   ].freeze
 
   # COLLECTION_FILTERS
